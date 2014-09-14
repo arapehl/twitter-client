bcbc1caefc275d8a0500b547bc77cb084110065e
Ara Pehlivanian <ara.pehlivanian@gmail.com>
2014-09-14 18:35:10 -0400

    successful authentication

---
+++ src/server/authenticate.js
@@ -0,0 +1,74 @@
+(function () {
+  'use strict';
+
+  var keys = require('./keys');
+  var OAuth = require('oauth').OAuth,
+      oauth = new OAuth(
+        "https://api.twitter.com/oauth/request_token",
+        "https://api.twitter.com/oauth/access_token",
+        keys.api_key,
+        keys.api_secret,
+        "1.0",
+        //null,
+        "http://aras-twitter-client.com/auth/twitter/callback",
+        "HMAC-SHA1"
+      );
+  var express = require('express');
+  var session = require('express-session');
+  var app = express();
+
+  app.use(session({secret: 'ugh', cookie: {maxAge: 60000}}));
+
+
+  app.get('/auth/twitter', function(req, res) {
+   
+    oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results) {
+      if (error) {
+        console.log(error);
+        res.send("Authentication Failed!");
+      }
+      else {
+        req.session.oauth = {
+          token: oauth_token,
+          token_secret: oauth_token_secret
+        };
+        console.log(req.session.oauth);
+        res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token);
+      }
+    });
+   
+  });
+
+  app.get('/auth/twitter/callback', function(req, res, next) {
+   
+    if (req.session.oauth) {
+      req.session.oauth.verifier = req.query.oauth_verifier;
+      var oauth_data = req.session.oauth;
+   
+      oauth.getOAuthAccessToken(
+        oauth_data.token,
+        oauth_data.token_secret,
+        oauth_data.verifier,
+        function(error, oauth_access_token, oauth_access_token_secret, results) {
+          if (error) {
+            console.log(error);
+            res.send("Authentication Failure!");
+          }
+          else {
+            req.session.oauth.access_token = oauth_access_token;
+            req.session.oauth.access_token_secret = oauth_access_token_secret;
+            console.log(results, req.session.oauth);
+            res.send("Authentication Successful");
+            // res.redirect('/'); // You might actually want to redirect!
+          }
+        }
+      );
+    }
+    else {
+      res.redirect('/login'); // Redirect to login page
+    }
+   
+  });
+
+  app.listen(80);
+}());
